269c8ad53aa00e2627c49fa058666c83720ed3f7
Chris Patti <feoh@feoh.org>
2023-10-02 16:46:07 -0400

    Fix - create actually maybe kinda sorta plausible version of the ol-concourse-github-issue resource

--- concourse_github_issues_resource.py
+++ concourse_github_issues_resource.py
@@ -1,11 +1,25 @@
-from concoursetools.additional import InOnlyConcourseResource  # type: ignore
+from concoursetools.additional import ConcourseResource  # type: ignore
 from github import Github, Auth  # type: ignore
 from os import environ
 
 
-class ConcourseGithubIssuesResource(InOnlyConcourseResource):
+class ConcourseGithubIssuesResource(ConcourseResource):
     def __init__(self):
-        auth = Auth.Token(environ["GITHUB_AUTH_TOKEN"])
-        gh = Github(auth=auth)
-        repo = gh.get_repo("mitodl/pipeline_workflow_repo")
-        repo.get_issues(state="open", labels=["pipeline"])
+        self.auth = Auth.Token(environ["GITHUB_AUTH_TOKEN"])
+        self.gh = Github(auth=self.auth)
+        self.repo = self.gh.get_repo("mitodl/pipeline_workflow_repo")
+        self.issue_title_prefix = ""
+        self.found_pipeline_issues = []
+
+    def fetch_new_versions(self, previous_version=None):
+        all_pipeline_issues = self.repo.get_issues(state="open", labels=["pipeline"])
+        matching_issues = [
+            issue
+            for issue in all_pipeline_issues
+            if issue.title.startswith(self.issue_title_prefix)
+        ]
+        if matching_issues in self.found_pipeline_issues:
+            return [previous_version]
+        else:
+            self.found_pipeline_issues = matching_issues
+            return self.found_pipeline_issues
