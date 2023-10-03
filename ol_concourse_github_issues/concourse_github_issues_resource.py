2881cd16f6337bc0535fb608854624b198fc9578
Chris Patti <feoh@feoh.org>
2023-10-03 15:14:40 -0400

    Fix - pull github auth token from Concourse resource definition YAML

--- ol_concourse_github_issues/concourse_github_issues_resource.py
+++ ol_concourse_github_issues/concourse_github_issues_resource.py
@@ -1,21 +1,31 @@
+"""
+resources:
+  - name: my-concourse-github-issues
+    type: concourse-github-issues
+    source:
+      github_auth_token: ((github.auth_token))
+      project_key: concourse
+      repo: "mitodl/my-project"
+
+"""
 from concoursetools.additional import ConcourseResource  # type: ignore
 from concoursetools.version import TypedVersion  # type: ignore
-from github import Github, Auth  # type: ignore
-from os import environ
+from github import Github  # type: ignore
 from datetime import datetime
+from dataclasses import dataclass
 
 
+@dataclass
 class ConcourseGithubIssuesVersion(TypedVersion):
-    issue_title: str
     issue_dt: datetime
 
 
 class ConcourseGithubIssuesResource(ConcourseResource):
-    def __init__(self):
-        self.auth = Auth.Token(environ["GITHUB_AUTH_TOKEN"])
-        self.gh = Github(auth=self.auth)
-        self.repo = self.gh.get_repo("mitodl/pipeline_workflow_repo")
-        self.issue_title_prefix = ""
+    def __init__(self, github_auth_token, repo, issue_title_prefix):
+        super().__init__(ConcourseGithubIssuesVersion)
+        self.gh = Github(auth=github_auth_token)
+        self.repo = self.gh.get_repo(repo)
+        self.issue_title_prefix = issue_title_prefix
         self.found_pipeline_issues = []
 
     def fetch_new_versions(self, previous_version=None):
