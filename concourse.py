01accf13e17537c82def84fef9371213ba949f33
Tobias Macey <tmacey@mit.edu>
2023-10-18 15:28:54 -0400

    fix: Properly hydrate and dehydrate Version objects

--- concourse.py
+++ concourse.py
@@ -8,17 +8,23 @@ resources:
       repo: "mitodl/my-project"
 
 """
-from typing import Optional
-from concoursetools.additional import ConcourseResource  # type: ignore
-from concoursetools.version import TypedVersion  # type: ignore
-from github import Github, Auth, Issue  # type: ignore
-from datetime import datetime
-from dataclasses import dataclass
+from typing import Literal, Optional
+from concoursetools import ConcourseResource
+from concoursetools.version import Version, SortableVersionMixin
+from github import Github, Auth, Issue
 
 
-@dataclass
-class ConcourseGithubIssuesVersion(TypedVersion):
-    issue_dt: datetime
+class ConcourseGithubIssuesVersion(Version, SortableVersionMixin):
+    def __init__(
+        self, issue_number: int, issue_title: str, issue_state, issue_created_at: str
+    ):
+        self.issue_number = issue_number
+        self.issue_title = issue_title
+        self.issue_created_at = issue_created_at
+        self.issue_state = issue_state
+
+    def __lt__(self, other: "ConcourseGithubIssuesVersion"):
+        return self.issue_number < other.issue_number
 
 
 class ConcourseGithubIssuesResource(ConcourseResource):
@@ -26,19 +32,32 @@ class ConcourseGithubIssuesResource(ConcourseResource):
         self,
         access_token: str,
         repository: str,
+        issue_state: Literal["open", "closed"] = "closed",
         issue_prefix: Optional[str] = None,
         labels: Optional[list[str]] = None,
     ):
         super().__init__(ConcourseGithubIssuesVersion)
         self.gh = Github(auth=Auth.Token(access_token))
         self.repo = self.gh.get_repo(repository)
+        self.issue_state = issue_state
         self.issue_prefix = issue_prefix
         self.found_pipeline_issues: list[Issue] = []
         self.issue_labels = labels
 
+    def _to_version(self, gh_issue: Issue):
+        return ConcourseGithubIssuesVersion(
+            issue_number=gh_issue.number,
+            issue_title=gh_issue.title,
+            issue_state=gh_issue.state,
+            issue_created_at=gh_issue.created_at.strftime("%Y-%m-%dT%H:%M:%SZ"),
+        )
+
+    def _from_version(self, version: ConcourseGithubIssuesVersion):
+        return self.repo.get_issue(version.issue_number)
+
     def fetch_new_versions(self, previous_version=None):
         all_pipeline_issues = self.repo.get_issues(
-            state="open", labels=self.issue_labels or []
+            state=self.issue_state, labels=self.issue_labels or []
         )
         matching_issues = [
             issue
@@ -49,7 +68,7 @@ class ConcourseGithubIssuesResource(ConcourseResource):
             return [previous_version]
         else:
             self.found_pipeline_issues = matching_issues
-            return self.found_pipeline_issues
+            return [self._to_version(issue) for issue in self.found_pipeline_issues]
 
     def download_version(self, version, destination_dir, build_metadata):
         pass
