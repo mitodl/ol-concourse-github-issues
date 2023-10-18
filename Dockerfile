246f4193b6dc12fc4ccb92fbe5c81ab6749ec5c4
Tobias Macey <tmacey@mit.edu>
2023-10-18 14:18:33 -0400

    feat: Allow configuring labels in resource definition
    - Add some type hints to clarify what parameters get what types
    - Allow passing a list of issue labels for filtering issues
    - Update Dockerfile to use a less privileged user

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
-FROM python:3.12-bookworm
-
+FROM python:3.11-slim-bookworm
+LABEL mitodl/ol-concourse-github-issues
+RUN mkdir -p /opt/resource/ && useradd -b /opt/resource/ -s /bin/false -M app && chown -R app:app /opt/resource
 COPY requirements.txt requirements.txt
 
 RUN python3 -m pip install --upgrade pip && \
-    pip install -r requirements.txt --no-deps
+    pip install -r requirements.txt --no-deps --no-cache-dir
 
+USER app
 WORKDIR /opt/resource/
 COPY concourse.py ./concourse.py
 RUN python3 -m concoursetools . -r concourse.py
--- concourse.py
+++ concourse.py
@@ -8,9 +8,10 @@ resources:
       repo: "mitodl/my-project"
 
 """
+from typing import Optional
 from concoursetools.additional import ConcourseResource  # type: ignore
 from concoursetools.version import TypedVersion  # type: ignore
-from github import Github, Auth  # type: ignore
+from github import Github, Auth, Issue  # type: ignore
 from datetime import datetime
 from dataclasses import dataclass
 
@@ -21,17 +22,24 @@ class ConcourseGithubIssuesVersion(TypedVersion):
 
 
 class ConcourseGithubIssuesResource(ConcourseResource):
-    def __init__(self, access_token, repository, issue_prefix):
+    def __init__(
+        self,
+        access_token: str,
+        repository: str,
+        issue_prefix: Optional[str] = None,
+        labels: Optional[list[str]] = None,
+    ):
         super().__init__(ConcourseGithubIssuesVersion)
-        print(f"ConcourseGithubIssuesResource:init: {repository=}")
-        print(f"ConcourseGithubIssuesResource:init: {issue_prefix=}")
         self.gh = Github(auth=Auth.Token(access_token))
         self.repo = self.gh.get_repo(repository)
         self.issue_prefix = issue_prefix
-        self.found_pipeline_issues = []
+        self.found_pipeline_issues: list[Issue] = []
+        self.issue_labels = labels
 
     def fetch_new_versions(self, previous_version=None):
-        all_pipeline_issues = self.repo.get_issues(state="open", labels=["pipeline"])
+        all_pipeline_issues = self.repo.get_issues(
+            state="open", labels=self.issue_labels
+        )
         matching_issues = [
             issue
             for issue in all_pipeline_issues
